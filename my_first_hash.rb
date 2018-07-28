a16629407b5e3e8ab0d35f7deac85964e9788847
Rafael Lopez <lpz.raf@gmail.com>
2018-07-28 02:38:27 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,8 +25,8 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-shipping_manifest[2]
 end
+shipping_manifest[2]
 
 def adding
   shipping_manifest = {
