3b3abfd549cad0391d9d5751e9f2207641003e74
Rafael Lopez <lpz.raf@gmail.com>
2018-07-28 02:33:56 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-my_hash = 
+my_hash = {hash1 => "hash1", hash2 => "hash2"}
   
 end
 
